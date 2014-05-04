cc8189461f8e59cf62a1cdf1cd2fc837ee6add57
parin2092 <parin2092@gmail.com>
2014-05-04 21:49:02 +0530

    ALL hail me

--- js/background.js
+++ js/background.js
@@ -8,94 +8,95 @@
 //     'css': ''
 // };
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    myScripter(tab, true);
-});
+chrome.browserAction.onClicked.addListener( function ( tab ) {
+    myScripter( tab, true );
+} );
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener( function ( tabId, changeInfo, tab ) {
     try {
-        if (changeInfo.status == 'complete') {
-            myScripter(tab, false);
+        if ( changeInfo.status == 'complete' ) {
+            myScripter( tab, false );
         }
-    } catch (err) {
-        logger('some error::' + err.message);
+    } catch ( err ) {
+        console.log( 'some error::' + err.message );
     }
-});
+} );
 
-function logger(stmt) {
-    console.log("******myscripter:: ");
-    console.log(stmt);
-}
+// function console.log( stmt ) {
+//     console.log( "******myscripter:: " );
+//     console.log( stmt );
+// }
 
-function myScripter(tab, popUpClicked) {
-    logger(tab);
+function myScripter( tab, popUpClicked ) {
+    console.log( tab );
     tabId = tab.id;
-    var prjmyindexes_9 = $.jStorage.get('prjmyindexes_9');
+    var prjmyindexes_9 = $.jStorage.get( 'prjmyindexes_9' );
     // var arr = tab.url.split('://');
 
-    url_regexes = Object.keys(prjmyindexes_9);
+    url_regexes = Object.keys( prjmyindexes_9 );
 
-    var ext_js_func = function(tabId, ext_js) {
-        chrome.tabs.executeScript(tabId, {
+    var ext_js_func = function ( tabId, ext_js ) {
+        console.log( "******JAY MATA DEE******" )
+        chrome.tabs.executeScript( tabId, {
             code: ext_js
-        }, function() {
-            logger('External JS insertion completed');
-        });
+        }, function ( ) {
+            console.log( 'External JS insertion completed' );
+        } );
     };
 
     // loop through localstorage to match url
-    $.each(url_regexes, function(u, regex_url) {
-        var cur_regex = new RegExp(regex_url);
-        console.log(cur_regex);
-        if (!cur_regex.test(tab.url)) {
+    $.each( url_regexes, function ( u, regex_url ) {
+        var cur_regex = new RegExp( regex_url );
+        console.log( cur_regex );
+        if ( !cur_regex.test( tab.url ) ) {
             return true;
         }
 
-        $.each(prjmyindexes_9[regex_url], function(key, value) {
-            var idsx = parseInt(value, 10);
-            logger("[start] gettng data for " + idsx);
-            var d = $.jStorage.get(idsx);
-            logger(d);
-            logger("[end] gettng data for " + idsx);
+        $.each( prjmyindexes_9[ regex_url ], function ( key, value ) {
+            var idsx = parseInt( value, 10 );
+            console.log( "[start] gettng data for " + idsx );
+            var d = $.jStorage.get( idsx );
+            console.log( d );
+            console.log( "[end] gettng data for " + idsx );
 
             // when url is matched against localstorage
 
-            if (false === d.autoApply && false == popUpClicked) {
-                logger(cur_regex + " - url has autoApply as false.");
+            if ( false === d.autoApply && false == popUpClicked ) {
+                console.log( cur_regex + " - url has autoApply as false." );
                 return false;
             }
 
-            ext_js_code = '\nvar myScripterI=0;\n inline_js_func=function(){var script = document.createElement("script");script.textContent = "' + d.js + '";document.body.appendChild(script);};';
+            ext_js_code = '\nvar myScripterI=0;\n inline_js_func=function(){var script = document.createElement("script");script.textContent = ' + d.js + ';document.body.appendChild(script);};';
             ext_js_flag = false;
 
-            $.each(d.external.js, function(u, v) {
+            $.each( d.external.js, function ( u, v ) {
                 ext_js_flag = true;
                 ext_js_code += 'var s;\ns = document.createElement("script");\nmyScripterI +=1;\ns.src = "' + v + '";\ndocument.body.appendChild(s);\ns.onload=function(){\nmyScripterI-= 1;\nconsole.log("script loaded " +"' + v + '");\nif(myScripterI == 0){\nconsole.log("execute inline js code ");\n\n inline_js_func();}};\n\n';
-            });
+            } );
 
-            $.each(d.external.css, function(u, v) {
+            $.each( d.external.css, function ( u, v ) {
                 ext_js_code += 'var s;\nl = document.createElement("link");\n l.setAttribute("rel", "stylesheet");l.setAttribute("type", "text/css");l.setAttribute("href", "' + v + '");\ndocument.body.appendChild(l);';
-            });
+            } );
 
-            if ("" !== ext_js_code) {
-                if (false == ext_js_flag)
-                    ext_js_code += "\n inline_js_func();";
+            if ( "" !== ext_js_code ) {
+                if ( false == ext_js_flag )
+                    ext_js_code += "\n setTimeout(inline_js_func,0);";
 
-                logger('this is my js code ');
-                logger(ext_js_code);
-                ext_js_func(tabId, ext_js_code);
+                console.log( 'this is my js code ' );
+                console.log( ext_js_code );
+                ext_js_func( tabId, ext_js_code );
             }
 
-            if ("" !== d.css) {
-                chrome.tabs.insertCSS(tabId, {
+            if ( "" !== d.css ) {
+                chrome.tabs.insertCSS( tabId, {
                     code: d.css,
                     runAt: 'document_start'
-                }, function() {
-                    logger('inline css insertion completed');
-                });
+                }, function ( ) {
+                    console.log( 'inline css insertion completed' );
+                } );
             }
-        });
+        } );
 
 
-    });
+    } );
 }
--- js/options.js
+++ js/options.js
@@ -1,15 +1,28 @@
-// var MyScriptsModule = angular.module('MyScriptsModule', []);
-function MyScriptsController($scope) {
-    $scope.view_mode = 'search_mode';
-    $('.alert_box').hide();
+// $( document ).ready( function ( ) {
+//     var obj = {
+//         a: 123,
+//         b: "4 5 6"
+//     };
 
-    $scope.create_new = function() {
+//     var data = "text/json;charset=utf-8," + encodeURIComponent( JSON.stringify( obj ) );
+
+//     $( '<a type="button" class="btn btn-info" href="data:' + data + '" download="parin.json">Export</a>' ).appendTo( '#download' );
+
+// } );
+// var MyScriptsModule = angular.module('MyScriptsModule', []);
+
+function MyScriptsController( $scope ) {
+    $scope.view_mode = 'search_mode';
+    $( '.alert_box' ).hide( );
+
+
+    $scope.create_new = function ( ) {
         $scope.cur_project = {
             name: '',
             url: '',
             external: {
-                js: [],
-                css: []
+                js: [ ],
+                css: [ ]
             },
             js: '',
             css: '',
@@ -21,121 +34,121 @@ function MyScriptsController($scope) {
         $scope.cur_project_old_url = undefined;
     };
 
-    $scope.add_custom_js = function() {
+    $scope.add_custom_js = function ( ) {
         var custom_url = $scope.custom_js;
-        if (nullOrEmpty(custom_url)) {
+        if ( nullOrEmpty( custom_url ) ) {
             return;
         }
 
-        $scope.cur_project.external.js.push(custom_url);
+        $scope.cur_project.external.js.push( custom_url );
         $scope.custom_js = undefined;
     };
 
-    $scope.add_custom_css = function() {
+    $scope.add_custom_css = function ( ) {
         var custom_url = $scope.custom_css;
-        if (nullOrEmpty(custom_url)) {
+        if ( nullOrEmpty( custom_url ) ) {
             return;
         }
 
-        $scope.cur_project.external.css.push(custom_url);
+        $scope.cur_project.external.css.push( custom_url );
         $scope.custom_css = undefined;
     };
 
-    $scope.remove_external_js = function(key) {
-        $scope.cur_project.external.js.splice(key, 1);
+    $scope.remove_external_js = function ( key ) {
+        $scope.cur_project.external.js.splice( key, 1 );
     };
 
-    $scope.remove_external_css = function(key) {
-        $scope.cur_project.external.css.splice(key, 1);
+    $scope.remove_external_css = function ( key ) {
+        $scope.cur_project.external.css.splice( key, 1 );
     };
 
-    $scope.move_up = function(key, array) {
-        var value = array[key];
-        array[key] = array[key - 1];
-        array[key - 1] = value;
+    $scope.move_up = function ( key, array ) {
+        var value = array[ key ];
+        array[ key ] = array[ key - 1 ];
+        array[ key - 1 ] = value;
     };
 
-    $scope.move_down = function(key, array) {
-        var value = array[key];
-        array[key] = array[key + 1];
-        array[key + 1] = value;
+    $scope.move_down = function ( key, array ) {
+        var value = array[ key ];
+        array[ key ] = array[ key + 1 ];
+        array[ key + 1 ] = value;
     };
 
-    function nullOrEmpty(input) {
-        return ['', undefined, null].indexOf(input) > -1;
+    function nullOrEmpty( input ) {
+        return [ '', undefined, null ].indexOf( input ) > -1;
     }
 
-    $scope.save_project = function() {
-        if (nullOrEmpty($scope.cur_project.id)) {
-            $scope.cur_project.id = get_sequence();
+    $scope.save_project = function ( ) {
+        if ( nullOrEmpty( $scope.cur_project.id ) ) {
+            $scope.cur_project.id = get_sequence( );
         }
 
-        save_project($scope.cur_project.id, $scope.cur_project);
+        save_project( $scope.cur_project.id, $scope.cur_project );
 
-        var all_indexes = get_all_indexes();
+        var all_indexes = get_all_indexes( );
 
         var cur_url = $scope.cur_project.url;
 
-        if (nullOrEmpty(all_indexes[cur_url])) {
-            all_indexes[cur_url] = [];
+        if ( nullOrEmpty( all_indexes[ cur_url ] ) ) {
+            all_indexes[ cur_url ] = [ ];
         }
 
-        if (nullOrEmpty($scope.cur_project_old_url)) {
+        if ( nullOrEmpty( $scope.cur_project_old_url ) ) {
             $scope.cur_project_old_url = cur_url;
         }
 
-        if (all_indexes[cur_url].indexOf($scope.cur_project.id) == -1) {
-            all_indexes[cur_url].push($scope.cur_project.id);
+        if ( all_indexes[ cur_url ].indexOf( $scope.cur_project.id ) == -1 ) {
+            all_indexes[ cur_url ].push( $scope.cur_project.id );
         }
 
-        if (cur_url != $scope.cur_project_old_url && all_indexes[$scope.cur_project_old_url] && all_indexes[$scope.cur_project_old_url].indexOf($scope.cur_project.id) > -1) {
-            all_indexes[$scope.cur_project_old_url].splice(all_indexes[$scope.cur_project_old_url].indexOf($scope.cur_project.id), 1);
+        if ( cur_url != $scope.cur_project_old_url && all_indexes[ $scope.cur_project_old_url ] && all_indexes[ $scope.cur_project_old_url ].indexOf( $scope.cur_project.id ) > -1 ) {
+            all_indexes[ $scope.cur_project_old_url ].splice( all_indexes[ $scope.cur_project_old_url ].indexOf( $scope.cur_project.id ), 1 );
         }
 
-        save_all_indexes(all_indexes);
+        save_all_indexes( all_indexes );
 
     };
 
-    $scope.delete_project = function() {
-        if (!confirm('Are you sure you want to delete this project?')) {
+    $scope.delete_project = function ( ) {
+        if ( !confirm( 'Are you sure you want to delete this project?' ) ) {
             return;
         }
 
-        var all_indexes = get_all_indexes();
-        $.jStorage.deleteKey($scope.cur_project.id);
-        all_indexes[$scope.cur_project_old_url].splice(all_indexes[$scope.cur_project_old_url].indexOf($scope.cur_project.id), 1);
-        save_all_indexes(all_indexes);
+        var all_indexes = get_all_indexes( );
+        $.jStorage.deleteKey( $scope.cur_project.id );
+        all_indexes[ $scope.cur_project_old_url ].splice( all_indexes[ $scope.cur_project_old_url ].indexOf( $scope.cur_project.id ), 1 );
+        save_all_indexes( all_indexes );
 
-        $scope.close_screen();
+        $scope.close_screen( );
     };
 
-    $scope.get_project = function(id) {
-        $scope.cur_project = $.jStorage.get(id);
+    $scope.get_project = function ( id ) {
+        $scope.cur_project = $.jStorage.get( id );
         $scope.cur_project_old_url = $scope.cur_project.url;
         $scope.view_mode = 'edit_mode';
     };
 
-    $scope.close_screen = function() {
-        get_all_project_names();
+    $scope.close_screen = function ( ) {
+        get_all_project_names( );
         $scope.view_mode = 'search_mode';
     };
 
-    function get_all_projects() {
-        return nullOrEmpty($.jStorage.get('prjmyscripts_9')) ? {} : $.jStorage.get('prjmyscripts_9');
+    function get_all_projects( ) {
+        return nullOrEmpty( $.jStorage.get( 'prjmyscripts_9' ) ) ? {} : $.jStorage.get( 'prjmyscripts_9' );
     }
 
-    function get_all_project_names() {
-        var prjmyindexes_9 = nullOrEmpty($.jStorage.get('prjmyindexes_9')) ? {} : $.jStorage.get('prjmyindexes_9');
+    function get_all_project_names( ) {
+        var prjmyindexes_9 = nullOrEmpty( $.jStorage.get( 'prjmyindexes_9' ) ) ? {} : $.jStorage.get( 'prjmyindexes_9' );
 
-        var project_ids = [];
-        for (url in prjmyindexes_9) {
-            var arr = prjmyindexes_9[url];
-            project_ids = project_ids.concat(arr);
+        var project_ids = [ ];
+        for ( url in prjmyindexes_9 ) {
+            var arr = prjmyindexes_9[ url ];
+            project_ids = project_ids.concat( arr );
         }
 
-        var project_names = [];
-        project_ids.forEach(function(id) {
-            var project = $.jStorage.get(id);
+        var project_names = [ ];
+        project_ids.forEach( function ( id ) {
+            var project = $.jStorage.get( id );
 
             var cur_project = {};
             cur_project.id = project.id;
@@ -143,60 +156,91 @@ function MyScriptsController($scope) {
             cur_project.url = project.url;
             cur_project.enabled = project.enabled;
 
-            project_names.push(cur_project);
-        });
+            project_names.push( cur_project );
+        } );
 
         $scope.all_project_names = project_names;
 
     };
 
-    get_all_project_names();
+    get_all_project_names( );
 
-    $scope.have_projects = function() {
+    $scope.have_projects = function ( ) {
         return $scope.all_project_names.length != 0;
     };
 
-    $scope.toggle_status = function(project) {
-        $scope.get_project(project.id);
+    $scope.toggle_status = function ( project ) {
+        $scope.get_project( project.id );
         $scope.cur_project.enabled = !$scope.cur_project.enabled;
-        $scope.save_project();
-        $scope.close_screen();
+        $scope.save_project( );
+        $scope.close_screen( );
     };
 
-    function save_all_projects(projects) {
-        $.jStorage.set('prjmyscripts_9', projects);
+    function save_all_projects( projects ) {
+        $.jStorage.set( 'prjmyscripts_9', projects );
     }
 
-    function save_project(id, project) {
-        $.jStorage.set(id, project);
-        $('.alert_box').show();
+    function save_project( id, project ) {
+        $.jStorage.set( id, project );
+        $( '.alert_box' ).show( );
 
-        setTimeout(function() {
-            $('.alert_box').hide();
-        }, 2500);
+        setTimeout( function ( ) {
+            $( '.alert_box' ).hide( );
+        }, 2500 );
         return true;
     }
 
-    function get_all_indexes() {
-        return nullOrEmpty($.jStorage.get('prjmyindexes_9')) ? {} : $.jStorage.get('prjmyindexes_9');
+    function get_all_indexes( ) {
+        return nullOrEmpty( $.jStorage.get( 'prjmyindexes_9' ) ) ? {} : $.jStorage.get( 'prjmyindexes_9' );
     }
 
-    function save_all_indexes(indexes) {
-        $.jStorage.set('prjmyindexes_9', indexes);
+    function save_all_indexes( indexes ) {
+        $.jStorage.set( 'prjmyindexes_9', indexes );
     }
 
-    $scope.show_help = function() {
-        $('#modal_help').modal('show');
+    $scope.show_help = function ( ) {
+        $( '#modal_help' ).modal( 'show' );
     };
 
-    function get_sequence() {
-        var seq = $.jStorage.get('sequence');
-        if (nullOrEmpty(seq)) {
-            $.jStorage.set('sequence', 1);
+    function get_sequence( ) {
+        var seq = $.jStorage.get( 'sequence' );
+        if ( nullOrEmpty( seq ) ) {
+            $.jStorage.set( 'sequence', 1 );
             return 1;
         } else {
-            $.jStorage.set('sequence', seq + 1);
+            $.jStorage.set( 'sequence', seq + 1 );
             return seq;
         }
     }
+
+    $scope.handleKeyBoardEvent = function ( ) {
+        if ( event.ctrlKey || event.metaKey ) {
+            // console.log( String.fromCharCode( event.which ).toLowerCase( ) )
+            switch ( String.fromCharCode( event.which ).toLowerCase( ) ) {
+            case 's':
+                if ( $scope.view_mode === "edit_mode" ) {
+                    $scope.save_project( );
+                    event.preventDefault( );
+                }
+                break;
+            case 'p':
+                event.preventDefault( );
+                $scope.create_new( );
+                break;
+            case 'h':
+                event.preventDefault( );
+                $scope.close_screen( );
+                break;
+            }
+        }
+    }
+    $scope.exportProject = function ( id ) {
+        // export monkey
+        var projects = get_all_projects( );
+        var data = "data:text/json;charset=utf-8," + encodeURIComponent( JSON.stringify( projects ) );
+        window.open( "_blank", data );
+        $scope.settings = data;
+        var data = $scope.get_project( id )
+    }
+
 };
