38865a00e31a68408dc609dd627f2612dd959e9e
parin2092 <parin2092@gmail.com>
2014-06-19 15:25:13 +0530

    changes

--- ms_baazar/app.js
+++ ms_baazar/app.js
@@ -66,24 +66,24 @@ app.post( '/comment', recipeController.comment );
 app.post( '/forked', metaController.forked );
 app.post( '/incUsersRecipes', metaController.incUsersRecipes );
 
-app.delete( '/r/saymyname', function ( request, response ) {
-    if ( request.body.heisenberg == "jessy" ) {
-        db.collections[ 'users' ].drop( function ( err ) {
-            console.log( 'collection dropped' );
-        } );
-        db.collections[ 'recipes' ].drop( function ( err ) {
-            console.log( 'collection dropped' );
-        } );
-        db.collections[ 'metas' ].drop( function ( err ) {
-            console.log( 'collection dropped' );
-        } );
-    } else {
-        response.json( {
-            "fuck": "you"
-        }, 500 )
-    }
+// app.delete( '/r/saymyname', function ( request, response ) {
+//     if ( request.body.heisenberg == "jessy" ) {
+//         db.collections[ 'users' ].drop( function ( err ) {
+//             console.log( 'collection dropped' );
+//         } );
+//         db.collections[ 'recipes' ].drop( function ( err ) {
+//             console.log( 'collection dropped' );
+//         } );
+//         db.collections[ 'metas' ].drop( function ( err ) {
+//             console.log( 'collection dropped' );
+//         } );
+//     } else {
+//         response.json( {
+//             "fuck": "you"
+//         }, 500 )
+//     }
 
-} );
+// } );
 
 var port = 3000;
 app.listen( port, function ( ) {
